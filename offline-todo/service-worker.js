4f6c4f133dc26c5c91c0fbaccf785038e3d70239
erdii <jg@tcs.de>
2016-11-27 00:04:01 +0100

    update serviceworker cache policy to get and update and added a timeout

--- offline-todo/service-worker.js
+++ offline-todo/service-worker.js
@@ -1,21 +1,56 @@
-self.addEventListener("install", (e) => {
-	e.waitUntil(
-		caches.open("notes.js").then((cache) => {
-			return cache.addAll([
-				"/",
-				"/index.html",
-				"/todo.js",
-				"/todo.css",
-				"/service-worker.js",
-			]);
-		})
-	)
+const KEY = "notes.js";
+
+self.addEventListener("install", (event) => {
+	console.log("installing serviceworker");
+
+	event.waitUntil(precache());
 });
 
-self.addEventListener("fetch", (e) => {
-	e.respondWith(
-		caches.match(e.request).then((cachedResponse) => (
-			cachedResponse || fetch(e.request)
-		))
-	);
+self.addEventListener("fetch", (event) => {
+	console.log("service worker is serving this request");
+
+	const { request } = event;
+
+	event.respondWith(fromNetwork(request, 400).catch(function () {
+		return fromCache(request);
+	}));
 });
+
+function precache() {
+	return caches.open(KEY).then((cache) => {
+		return cache.addAll([
+			"./",
+			"./index.html",
+			"./todo.js",
+			"./todo.css",
+		]);
+	});
+}
+
+function fromNetwork(request, timeout) {
+	return new Promise(function(fulfill, reject) {
+		let timeoutId = setTimeout(reject, timeout);
+
+		console.log("trying network");
+		fetch(request).then(function (response) {
+			clearTimeout(timeoutId);
+
+			caches.open(KEY).then(function (cache) {
+				console.log("updating cache");
+				cache.put(request, response);
+				cache.match(request).then(function (matching) {
+					fulfill(matching);
+				})
+			});
+		}, reject);
+	});
+}
+
+function fromCache(request) {
+	console.log("trying cache");
+	return caches.open(KEY).then(function (cache) {
+		return cache.match(request).then(function (matching) {
+			return matching || Promise.reject("no-match"); 
+		})
+	});
+}
